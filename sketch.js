045161c774212c4edf2d5175a2bc1cab1765dfd9
Archie <argiris153@gmail.com>
2025-01-09 23:34:48 +0200

    FullScreen For Menu

--- sketch.js
+++ sketch.js
@@ -295,3 +295,13 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     updateButtonPositions();
 }
+
+let fullscreenActivated = false;
+
+function mousePressed() {
+  if (!fullscreenActivated && mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+    fullscreenActivated = true; // Mark as activated
+  }
+}
